4f38ea420778681265618373d48788dbb4d86588
Alex Murray <murray.alex@gmail.com>
2013-05-20 08:58:30 +0930

    Enable ido-everywhere
    This uses ido for all buffer and file selection (ie. pop-to-buffer) etc

--- init-ido.el
+++ init-ido.el
@@ -10,6 +10,8 @@
 (setq ido-enable-flex-matching t
       ;; use ido virtual buffers to remember previously opened files
       ido-use-virtual-buffers t)
+;; use ido for buffer and filenames as well
+(ido-everywhere t)
 
 ;; when using ido, the confirmation is rather annoying...
 (setq confirm-nonexistent-file-or-buffer nil)
