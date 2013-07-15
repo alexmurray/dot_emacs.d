6ef4c2b6df4b6633f0cf25040006887d724b79b5
Alex Murray <murray.alex@gmail.com>
2013-07-15 04:02:14 +0000

    Semantic updates to work better with projects etc

--- init-semantic.el
+++ init-semantic.el
@@ -13,14 +13,31 @@
 ;; faster to compute next time a file is loaded
 (semantic-mode 1)
 (global-semanticdb-minor-mode 1)
+
 ;; show summary of tag at point when idle
 (global-semantic-idle-summary-mode 1)
 
-;; enable support for gnu global - to generate a global database for a
-;; project, something like the following works:
-;; find . -not -iwholename "/.svn" -name *.[ch] | gtags -f -
-(semanticdb-enable-gnu-global-databases 'c-mode)
-(semanticdb-enable-gnu-global-databases 'c++-mode)
+;; get semantic to index back to various top-level marker files
+(defvar apm-semantic-project-root-markers
+ '(".git" "cohda-version" "GTAGS" "TAGS" "Makefile"))
+
+(setq semanticdb-project-root-functions
+      (mapcar #'(lambda (file)
+		  (eval `(lambda (directory) (locate-dominating-file directory ,file))))
+	      apm-semantic-project-root-markers))
+
+;; setup for c, c++ and java
+(defvar apm-semantic-modes
+  '(c-mode c++-mode java-mode))
+
+(dolist (mode apm-semantic-modes)
+  (setq-mode-local mode
+		   semanticdb-find-default-throttle
+		   '(project unloaded system recursive omniscience))
+  ;; enable support for gnu global - to generate a global database for a
+  ;; project, something like the following works:
+  ;; find . -not -iwholename "/.svn" -name *.[ch] | gtags -f -
+  (semanticdb-enable-gnu-global-databases mode))
 
 (provide 'init-semantic)
 
