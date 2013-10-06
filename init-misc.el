7b0ac9d51516489ec8c1254b1278e5a3cc5628dc
Alex Murray <murray.alex@gmail.com>
2013-10-06 20:50:58 +1030

    Use Firefox instead of Chrome as default browser

--- init-misc.el
+++ init-misc.el
@@ -12,9 +12,9 @@
 ;; just use y or n not yes or no
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; use chrome as default broswer
+;; use firefox as default broswer
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "google-chrome")
+      browse-url-generic-program "firefox")
 
 ;; since we store .emacs in a symlinked git repo, always follow symlinks for vc
 (setq vc-follow-symlinks t)
