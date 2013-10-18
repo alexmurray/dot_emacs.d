c8deb6838ec0ed72cac53b2a81ca014f4daa907d
Alex Murray <murray.alex@gmail.com>
2013-10-18 14:19:57 +1030

    Use gtags for evil tag movement if enabled
    Also use normal emacs mode for a bunch of different modes rather than
    evil-normal-mode

--- init-evil.el
+++ init-evil.el
@@ -16,6 +16,27 @@
 (require 'evil-matchit)
 (global-evil-matchit-mode 1)
 
+(defadvice evil-jump-to-tag (around evil-gtags-jump-to-tag activate)
+  "Make use of gtags if possible instead of etags for finding definitions."
+  (cond
+   ((bound-and-true-p gtags-mode) (gtags-find-tag-from-here))
+   (t ad-do-it)))
+
+(dolist (mode '(bc-menu-mode
+                comint-mode
+                inferior-emacs-lisp-mode
+                ebib-entry-mode
+                ebib-index-mode
+                ebib-log-mode
+                gtags-select-mode
+                magit-branch-manager-mode
+                pylookup-mode
+                rdictcc-buffer-mode
+                semantic-symref-results-mode
+                shell-mode
+                term-mode))
+  (evil-set-initial-state mode 'emacs))
+
 (provide 'init-evil)
 
 ;;; init-evil.el ends here
