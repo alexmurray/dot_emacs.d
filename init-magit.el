f2e763fe5d216d6191429e8c6c119cc0af51cd17
Alex Murray <murray.alex@gmail.com>
2013-10-30 10:10:31 +1030

    Show magit-status as fullscreen with q to exit back to previous windows

--- init-magit.el
+++ init-magit.el
@@ -6,7 +6,22 @@
 
 ;;; Code:
 (require 'magit)
+
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; full screen magit-status
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restore the previous window config and kill the magit buffer."
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (provide 'init-magit)
 
