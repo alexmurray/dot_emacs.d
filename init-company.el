303631569480639440a28eece5b1f170d2247e8a
Alex Murray <murray.alex@gmail.com>
2014-03-07 23:49:59 +1030

    Add some better keybindings for company-mode

--- init-company.el
+++ init-company.el
@@ -7,6 +7,13 @@
 ;;; Code:
 (require 'company)
 
+;; add some better keybindings for company
+(define-key company-active-map (kbd "\C-n") 'company-select-next)
+(define-key company-active-map (kbd "\C-p") 'company-select-previous)
+(define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "<tab>") 'company-complete)
+
+;; enable company in all buffers
 (add-hook 'after-init-hook 'global-company-mode)
 
 (eval-after-load "diminish"
