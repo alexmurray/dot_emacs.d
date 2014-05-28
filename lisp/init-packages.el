0dd623e73ed7511b277fe76a25d9b20319aec6dd
Alex Murray <murray.alex@gmail.com>
2014-05-28 09:26:59 -0400

    Move to just using melpa for all packages

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -8,8 +8,6 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
