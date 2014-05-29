4751fb71211fd5ccddf40aebb6d34184888fe23f
Alex Murray <murray.alex@gmail.com>
2014-05-29 10:39:25 -0400

    Make ethan-wspace less spammy of warnings

--- lisp/init-ethan-wspace.el
+++ lisp/init-ethan-wspace.el
@@ -6,6 +6,10 @@
 ;;; Code:
 (require 'ethan-wspace)
 
+;; ethan-wspace-mode raises lots of warnings if this is enabled...
+;; hopefully this doesn't cause problems
+(setq mode-require-final-newline nil)
+
 (global-ethan-wspace-mode 1)
 
 (eval-after-load 'diminish
