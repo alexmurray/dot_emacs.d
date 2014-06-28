7f17083a773fac79c329895bc8106f4c942fa001
Alex Murray <alexmurray@fastmail.fm>
2014-06-28 22:53:21 +0930

    Complete dfunc to infer printf formats from type names

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -6,55 +6,64 @@
 
 ;;; Code:
 
+(defun apm-c-type-to-format (type)
+  "Return a printf format directive for TYPE."
+  (cond ((or (null type)
+	     (string= type ""))
+	 nil)
+	((string-match "\\*" type)
+	 "%p")
+	((cl-some #'(lambda (r) (string-match r type))
+		 '("u\\(char\\|int\\|long\\)" "unsigned"))
+	 "%u")
+	((cl-some #'(lambda (r) (string-match r type))
+		 '("\\(char\\|int\\|long\\)"))
+	 "%d")
+	((cl-some #'(lambda (r) (string-match r type))
+		 '("\\(float\\|double\\)"))
+	 "%f")
+	;; use an invalid printf directive if none found
+	(t "%?")))
+
+(defvar apm-c-variable-decl-regex
+  "\\([[:alpha:][:space:]\\*_]+[[:space:]\\*]+\\)\\([[:alpha:]_][[:alnum:]_]*\\)")
+
 ;; useful functions for parsing out c function type arguments used in
 ;; c snippets for yasnippet
 (defun apm-c-get-function-param-names (param-string)
-  "Return a list of the parameter names from PARAM-STRING."
-  (let ((names nil))
-    (unless (string= param-string "void")
-      ;; split into individual parameter groups
-      (dolist (param (split-string param-string ","))
-        (let ((tokens (split-string param " ")))
-          ;; now find name of parameter as last element in each group with
-          ;; any leading * removed
-          (setq names (append names (list (replace-regexp-in-string "\\*" "" (car (last tokens))))))))
-      names)))
+  "Return a list of the parameter names from PARAM-STRING.
+PARAM-STRING should be as <typename> <variable>,..."
+  (unless (or (string= param-string "")
+	      (string= param-string "void"))
+    ;; get variable name from individual parameter groups
+    (mapcar 'apm-c-get-variable-name-from-declaration
+	    (split-string param-string ","))))
+
+(defun apm-c-get-function-param-types (param-string)
+  "Return a list of the parameter types from PARAM-STRING.
+PARAM-STRING should be as <typename> <variable>,..."
+  (unless (or (string= param-string "")
+	      (string= param-string "void"))
+    ;; get variable type from individual parameter groups
+    (mapcar 'apm-c-get-variable-type-from-declaration
+	    (split-string param-string ","))))
+
+(defun apm-c-get-function-param-formats (param-string)
+  "Return a list of the parameter printf formats from PARAM-STRING.
+PARAM-STRING should be as <typename> <variable>,..."
+  (mapcar 'apm-c-type-to-format (apm-c-get-function-param-types param-string)))
 
 (defun apm-c-get-variable-name-from-declaration (decl-string)
   "Return the name of the variable declared in DECL-STRING."
-  (let ((name nil)
-	(prev nil))
-    (dolist (token (split-string decl-string " "))
-      ;; keep track of prev token and current - if current is = then
-      ;; prev must be variable name - or if token ends in ; or , then
-      ;; this must be the variable name
-      (when (string= token "=")
-	(setq name prev))
-      (when (and (null name)
-		 (not (string= token ""))
-		 (or (string= ";" (substring token -1 nil))
-		     (string= "," (substring token -1 nil))))
-	(setq name (substring token 0 -1)))
-      (setq prev token))
-    ;; remove any * from name before returning
-    (when name
-      (replace-regexp-in-string "\\*" "" name))))
+  (if (string-match apm-c-variable-decl-regex decl-string)
+      (match-string 2 decl-string)
+    ""))
 
 (defun apm-c-get-variable-type-from-declaration (decl-string)
   "Return the type of the variable declared in DECL-STRING."
-  (let ((name (apm-c-get-variable-name-from-declaration decl-string))
-	(type nil)
-	(done nil))
-    (dolist (token (split-string decl-string " "))
-      (unless done
-	(if (string= (replace-regexp-in-string "\\*" "" token) name)
-	    (progn
-	      (while (string= (substring token 0 1) "*")
-		(concatenate 'string type "*")
-		(setq token (substring token 1 nil)))
-	      (setq done t))
-	  (setq type (concatenate 'string type token)))))
-    type))
+  (if (string-match apm-c-variable-decl-regex decl-string)
+      (match-string 1 decl-string)
+    ""))
 
 (defvar include-guard-format "__%s__"
   "Format string for include guard.
