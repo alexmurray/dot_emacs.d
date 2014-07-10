60fe056a24eca2365e4c5104ad8bfce2c1c4b9b0
Alex Murray <murray.alex@gmail.com>
2014-07-10 21:41:40 +0930

    Try using just smart company backends for c common modes

--- lisp/init-c-mode-common.el
+++ lisp/init-c-mode-common.el
@@ -74,7 +74,7 @@ code sections."
   ;; set company backends appropriately to prefer local dabbrev,
   ;; clang, gtags, semantic, yasnippet
   (eval-after-load 'company
-    '(setq-local company-backends '((company-semantic company-clang company-dabbrev-code company-yasnippet) company-gtags)))
+    '(setq-local company-backends '((company-semantic company-clang) company-gtags)))
   ;; show #if 0 / #endif etc regions in comment face
   (font-lock-add-keywords
    nil
