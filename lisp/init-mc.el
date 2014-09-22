8a8099a60a2619a5066009a0cfb9a6c2f600e1b6
Alex Murray <alex.murray@cohdawireless.com>
2014-09-22 09:41:40 +0930

    Try and make multiple cursors play better with evil

--- lisp/init-mc.el
+++ lisp/init-mc.el
@@ -6,6 +6,11 @@
 
 ;;; Code:
 (require 'multiple-cursors)
+(setq mc/unsupported-minor-modes '(company-mode flyspell-mode))
+(eval-after-load 'evil
+  '(progn
+     (add-hook 'multiple-cursors-mode-enabled-hook 'evil-emacs-state)
+     (add-hook 'multiple-cursors-mode-disabled-hook 'evil-normal-state)))
 
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
