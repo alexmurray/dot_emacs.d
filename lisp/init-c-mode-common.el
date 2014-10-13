68b552979f7275efe0c2a4ba30a22568168ceef1
Alex Murray <alexmurray@fastmail.fm>
2014-10-13 14:56:41 +1030

    Use better company backends for C common modes
    Prefer clang if possible then semantic, and finally
    gtags in a separate group so we get smart completions
    first if possible then 'dumb' ones since gtags is not
    context sensitive.

--- lisp/init-c-mode-common.el
+++ lisp/init-c-mode-common.el
@@ -86,7 +86,7 @@ code sections."
   ;; set company backends appropriately to prefer smart backends over
   ;; dumb and finish with yasnippet
   (eval-after-load 'company
-    '(setq-local company-backends '((company-semantic company-clang company-gtags))))
+    '(setq-local company-backends '((company-semantic company-clang) company-gtags)))
   ;; show #if 0 / #endif etc regions in comment face
   (font-lock-add-keywords
    nil
