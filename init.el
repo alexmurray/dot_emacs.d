e04d53e4c47cae67eaf7974ac6990fe902fba75e
Alex Murray <alexmurray@fastmail.fm>
2014-10-16 20:52:49 +1030

    Remove custom bits from init.el

--- init.el
+++ init.el
@@ -66,17 +66,3 @@
 (provide 'init)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
- '(magit-use-overlays nil)
- '(paradox-github-token t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
