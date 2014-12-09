46623b6481bb8815a72e1a9e8ba9770f6a2f26a7
Alex Murray <alex.murray@cohdawireless.com>
2014-12-10 09:46:25 +1030

    Add some nicer keybindings for company

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -15,7 +15,10 @@
 ;; align annotations in tooltip
 (setq company-tooltip-align-annotations t)
 
-;; nicer keybinding
+;; nicer keybindings
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
 (define-key company-active-map (kbd "<tab>") 'company-complete)
 
 ;; put most often used completions at stop of list
