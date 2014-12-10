af38b542d96db27e647481d05fb20f78d38fa0fb
Alex Murray <alex.murray@cohdawireless.com>
2014-12-11 09:52:26 +1030

    Make auto gtags smarter to not create if in home dir or user enters empty string

--- lisp/init-gtags.el
+++ lisp/init-gtags.el
@@ -21,27 +21,33 @@
     (when (cl-notany '(lambda (path) (string-match path dir))
                      apm-gtags-ignore-paths)
       (if (not (= 0 (call-process "global" nil nil nil " -p")))
-          ;; tagfile does not exist yet - prompt for where to create one
-          (let* ((dir (read-directory-name
-                       "gtags: top of source tree:" default-directory))
-                 (cmd (format
-                       "cd %s && gtags && echo 'Created GNU Global tag file'"
-                       dir))
-                 (process (start-process-shell-command "gtags-create" nil
-                                                       cmd)))
-            ;; display message about creating tags file
-            (message "Creating global tags file...")
-            (set-process-sentinel process
-                                  '(lambda (process event)
-                                     (message "Finished creating global tags file: %s" event))))
+          ;; tagfile does not exist yet - prompt for where to create
+          ;; one but not if already in home directory since will index
+          ;; too much
+          (let ((dir (if (string= default-directory
+                                  (substitute-in-file-name "$HOME/"))
+                         ""
+                       (read-directory-name
+                        "gtags: top of source tree:" default-directory))))
+            (if (string= dir "")
+                (message "Not creating new global tag file.")
+              (let ((process (start-process-shell-command "gtags-create" nil
+                                                          (format
+                                                           "cd %s && gtags && echo 'Created GNU Global tag file'"
+                                                           dir))))
+                ;; display message about creating tags file
+                (message "Creating global tags file...")
+                (set-process-sentinel process
+                                      '(lambda (process event)
+                                         (message "Finished creating global tags file: %s" event))))))
         ;; tagfile already exists - update it
         (let ((process (start-process-shell-command "gtags-update" nil
                                                     "global -u")))
-            ;; display message about updating tags file
-            (message "Updating global tags file...")
-            (set-process-sentinel process
-                                  '(lambda (process event)
-                                     (message "Finished updating global tags file: %s" event))))))))
+          ;; display message about updating tags file
+          (message "Updating global tags file...")
+          (set-process-sentinel process
+                                '(lambda (process event)
+                                   (message "Finished updating global tags file: %s" event))))))))
 
 (defvar apm-gtags-ignore-paths '("/usr/")
   "A list of paths to not automatically run apm-gtags-create-or-update.")
