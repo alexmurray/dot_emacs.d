bfd58e4a940d419fd787fd7bd6941786a1351478
Alex Murray <alex.murray@cohdawireless.com>
2015-06-30 16:43:10 +0930

    c-auto-newline is annoying I think...

--- init.el
+++ init.el
@@ -301,8 +301,6 @@ code sections."
     (diminish 'auto-fill-function " \uF036"))
   ;; turn on auto-newline and hungry-delete
   (c-toggle-auto-hungry-state t)
-  ;; set auto newline
-  (setq c-auto-newline 1)
   ;; make underscore a word character so movements across words
   ;; include it - this is the same as vim
   (modify-syntax-entry ?_ "w")
