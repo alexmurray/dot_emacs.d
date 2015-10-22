14000f756c4ea51b7013ac3ac493b86960b5cf39
Alex Murray <alex.murray@cohdawireless.com>
2015-10-22 12:20:01 +1030

    Use upstream helm-flx instead of own local copy

--- init.el
+++ init.el
@@ -748,38 +748,6 @@ Otherwise call `ediff-buffers' interactively."
   :defer t
   :init (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
 
-(use-package flx
-  :ensure t
-  ;; use flx with helm -
-  ;; https://www.reddit.com/r/emacs/comments/3o7a9i/using_helm_with_flx_for_better_fuzzy_matching
-  :config (with-eval-after-load 'helm
-            ;; this is a bit hackish, ATM, redefining functions I don't own
-            (defvar helm-flx-cache (flx-make-string-cache #'flx-get-heatmap-str))
-
-            (defun helm-score-candidate-for-pattern (candidate pattern)
-              (or (car (flx-score candidate pattern helm-flx-cache)) 0))
-
-            (defun helm-fuzzy-default-highlight-match (candidate)
-              (let* ((pair (and (consp candidate) candidate))
-                     (display (if pair (car pair) candidate))
-                     (real (cdr pair)))
-                (with-temp-buffer
-                  (insert display)
-                  (goto-char (point-min))
-                  (if (string-match-p " " helm-pattern)
-                      (cl-loop with pattern = (split-string helm-pattern)
-                               for p in pattern
-                               do (when (search-forward p nil t)
-                                    (add-text-properties
-                                     (match-beginning 0) (match-end 0) '(face helm-match))))
-                    (cl-loop with pattern = (cdr (flx-score display
-                                                            helm-pattern helm-flx-cache))
-                             for index in pattern
-                             do (add-text-properties
-                                 (1+ index) (+ 2 index) '(face helm-match))))
-                  (setq display (buffer-string)))
-                (if real (cons display real) display)))))
-
 (use-package flyspell
   :diminish flyspell-mode)
 
@@ -793,6 +761,11 @@ Otherwise call `ediff-buffers' interactively."
 (use-package gitignore-mode
   :ensure t
   :defer t)
+
+;; enable hlm-flx before helm
+(use-package helm-flx
+  :ensure t
+  :init (helm-flx-mode t))
 
 (use-package helm
   :ensure t
