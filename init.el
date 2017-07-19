37de227a37fc9ab70416ceceaa98d2936ffa7a2d
Alex Murray <murray.alex@gmail.com>
2017-07-19 12:53:23 +0930

    Go back to gruvbox - has nicer xml colors and works with spaceline

--- init.el
+++ init.el
@@ -1117,6 +1117,9 @@ Otherwise call `ediff-buffers' interactively."
   :defer t
   :init (add-hook 'prog-mode-hook #'goto-address-prog-mode))
 
+(use-package gruvbox-theme
+  :ensure t)
+
 (use-package gud
   :defer t
   :init (add-hook 'gud-mode-hook #'gud-tooltip-mode))
@@ -1831,6 +1834,7 @@ ${3:Ticket: #${4:XXXX}}")))
 
 (use-package zenburn-theme
   :ensure t
+  :disabled t
   :config (progn
             (load-theme 'zenburn t)
             ;; nicer looking modeline
