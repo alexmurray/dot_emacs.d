a1ac3e325b264e3d8a2a82ff8c2313c43a95683a
Alex Murray <murray.alex@gmail.com>
2018-03-28 12:11:57 +1030

    cquery does highlighting of #if 0 regions so get rid of custom

--- init.el
+++ init.el
@@ -414,33 +414,6 @@
   :defer t
   :init (add-hook 'rust-mode-hook 'cargo-minor-mode))
 
-;; show #if 0 / #endif etc regions in comment face - taken from
-;; http://stackoverflow.com/questions/4549015/in-c-c-mode-in-emacs-change-face-of-code-in-if-0-endif-block-to-comment-fa
-(defun c-mode-font-lock-if0 (limit)
-  "Fontify #if 0 / #endif as comments for c modes etc.
-Bound search to LIMIT as a buffer position to find appropriate
-code sections."
-  (save-restriction
-    (widen)
-    (save-excursion
-      (goto-char (point-min))
-      (let ((depth 0) str start start-depth)
-        (while (re-search-forward "^\\s-*#\\s-*\\(if\\|else\\|endif\\)" limit 'move)
-          (setq str (match-string 1))
-          (if (string= str "if")
-              (progn
-                (setq depth (1+ depth))
-                (when (and (null start) (looking-at "\\s-+0"))
-                  (setq start (match-end 0)
-                        start-depth depth)))
-            (when (and start (= depth start-depth))
-              (c-put-font-lock-face start (match-beginning 0) 'font-lock-comment-face)
-              (setq start nil))
-            (when (string= str "endif")
-              (setq depth (1- depth)))))
-        (when (and start (> depth 0))
-          (c-put-font-lock-face start (point) 'font-lock-comment-face)))))
-  nil)
 
 ;; c-mode and other derived modes (c++, java etc) etc
 (defun apm-c-mode-common-setup ()
@@ -460,12 +433,7 @@ code sections."
   (with-eval-after-load 'smartparens
     (local-set-key (kbd "C-)") 'sp-slurp-hybrid-sexp)
     (local-set-key (kbd "C-<right>") 'sp-slurp-hybrid-sexp)
-    (local-set-key (kbd "C-<left>") 'sp-dedent-adjust-sexp))
-
-  ;; show #if 0 / #endif etc regions in comment face
-  (font-lock-add-keywords
-   nil
-   '((c-mode-font-lock-if0 (0 font-lock-comment-face prepend))) 'add-to-end))
+    (local-set-key (kbd "C-<left>") 'sp-dedent-adjust-sexp)))
 
 (use-package cc-mode
   :defer t
