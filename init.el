38494745fc46ff7d6e1f9def3eb41b6612c61c59
Alex Murray <murray.alex@gmail.com>
2019-03-27 14:27:25 +1030

    Add company-box for much prettier completion display

--- init.el
+++ init.el
@@ -567,6 +567,57 @@ The object labels of the found items are returned as list."
   :defer t
   :hook ((LaTeX-mode . company-auctex-init)))
 
+(use-package company-box
+  :ensure t
+  :diminish company-box-mode
+  :hook ((company-mode . company-box-mode))
+  :config
+  (setq company-box-backends-colors nil)
+      (setq company-box-show-single-candidate t)
+      (setq company-box-max-candidates 50)
+      (setq company-box-icons-alist 'company-box-icons-all-the-icons)
+      (defun company-box-icons--elisp (candidate)
+        (when (derived-mode-p 'emacs-lisp-mode)
+          (let ((sym (intern candidate)))
+            (cond ((fboundp sym) 'Function)
+                  ((featurep sym) 'Module)
+                  ((facep sym) 'Color)
+                  ((boundp sym) 'Variable)
+                  ((symbolp sym) 'Text)
+                  (t . nil)))))
+
+      (with-eval-after-load 'all-the-icons
+        (declare-function all-the-icons-faicon 'all-the-icons)
+        (declare-function all-the-icons-material 'all-the-icons)
+        (setq company-box-icons-all-the-icons
+              `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.9 :v-adjust -0.2))
+                (Text . ,(all-the-icons-material "text_fields" :height 0.9 :v-adjust -0.2))
+                (Method . ,(all-the-icons-faicon "cube" :height 0.9 :v-adjust -0.06 :face 'all-the-icons-purple))
+                (Function . ,(all-the-icons-faicon "cube" :height 0.9 :v-adjust -0.06 :face 'all-the-icons-purple))
+                (Constructor . ,(all-the-icons-faicon "cube" :height 0.9 :v-adjust -0.06 :face 'all-the-icons-purple))
+                (Field . ,(all-the-icons-faicon "tag" :height 0.9 :v-adjust -0.06 :face 'all-the-icons-blue))
+                (Variable . ,(all-the-icons-faicon "tag" :height 0.9 :v-adjust -0.06 :face 'all-the-icons-blue))
+                (Class . ,(all-the-icons-material "settings_input_component" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
+                (Interface . ,(all-the-icons-material "share" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-blue))
+                (Module . ,(all-the-icons-material "view_module" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-blue))
+                (Property . ,(all-the-icons-faicon "wrench" :height 0.9 :v-adjust -0.06))
+                (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.9 :v-adjust -0.2))
+                (Value . ,(all-the-icons-material "format_align_right" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-blue))
+                (Enum . ,(all-the-icons-material "storage" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
+                (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.9 :v-adjust -0.2))
+                (Snippet . ,(all-the-icons-material "format_align_center" :height 0.9 :v-adjust -0.2))
+                (Color . ,(all-the-icons-material "palette" :height 0.9 :v-adjust -0.2))
+                (File . ,(all-the-icons-faicon "file-o" :height 0.9 :v-adjust -0.06))
+                (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.9 :v-adjust -0.2))
+                (Folder . ,(all-the-icons-faicon "folder-open" :height 0.9 :v-adjust -0.06))
+                (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-blueb))
+                (Constant . ,(all-the-icons-faicon "square-o" :height 0.9 :v-adjust -0.06))
+                (Struct . ,(all-the-icons-material "settings_input_component" :height 0.9 :v-adjust -0.2 :face 'all-the-icons-orange))
+                (Event . ,(all-the-icons-faicon "bolt" :height 0.9 :v-adjust -0.06 :face 'all-the-icons-orange))
+                (Operator . ,(all-the-icons-material "control_point" :height 0.9 :v-adjust -0.2))
+                (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.9 :v-adjust -0.06))
+                (Template . ,(all-the-icons-material "format_align_center" :height 0.9 :v-adjust -0.2))))))))
+
 (use-package company-dabbrev
   :after company
   ;; keep original case
