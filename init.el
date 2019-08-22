e9c64d9c060d9c7efd4297918e556e47819c2439
Alex Murray <murray.alex@gmail.com>
2019-08-22 15:33:56 +0930

    Show org-pomodoro alerts as persistent notifications

--- init.el
+++ init.el
@@ -1904,7 +1904,12 @@ This will replace the last notification sent with this function."
   (setq org-pomodoro-clock-break t)
   (setq org-pomodoro-play-sounds nil)
   (setq org-pomodoro-keep-killed-pomodoro-time t)
-  (setq org-pomodoro-ask-upon-killing nil))
+  (setq org-pomodoro-ask-upon-killing nil)
+  ;; ensure alerts from org-pomodoro get shown via dbus notifications and
+  ;; are persistent
+  (add-to-list 'alert-user-configuration '(((:category . "org-pomodoro"))
+                                           notifications
+                                           ((:persistent t)))))
 
 (use-package org-protocol
   :ensure org-plus-contrib
