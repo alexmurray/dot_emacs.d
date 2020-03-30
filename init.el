2e53277bf4ee16c501e1ffec615dbeaf1e0f0949
Alex Murray <murray.alex@gmail.com>
2020-03-31 10:04:22 +1030

    Show org clock table at level 3 by default rather than 2

--- init.el
+++ init.el
@@ -1851,7 +1851,8 @@ With a prefix argument, will default to looking for all
         org-adapt-indentation nil
         org-imenu-depth 4
         org-todo-keywords '((sequence "TODO(t)" "IN-PROGRESS(p!)" "BLOCKED(b@)" "DEFERRED(D@)" "|" "DONE(d!)")
-                            (sequence "|" "CANCELLED(c@)" "DELEGATED(G@)")))
+                            (sequence "|" "CANCELLED(c@)" "DELEGATED(G@)"))
+        org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3))
   (add-to-list 'org-file-apps '("\\.webm\\'" . "xdg-open %s"))
   (add-to-list 'org-file-apps '("\\.aup\\'" . "audacity %s"))
   ;; set up org-babel integration for plantuml
