9ccc4a23ac45809940a383dc032aed6f911faae7
Alex Murray <murray.alex@gmail.com>
2020-04-23 21:37:08 +0930

    Handle apm-occur-in-erc with nothing at point

--- init.el
+++ init.el
@@ -812,12 +812,13 @@ With a prefix argument, will default to looking for all
                             "\\)")))
         (read-string "Regexp: "
                      (substring-no-properties
-                      (cond ((region-active-p)
+                      (or (cond ((region-active-p)
                              (buffer-substring (region-beginning) (region-end)))
                             (current-prefix-arg
                              regex)
                             (t
-                             (word-at-point))))))))
+                             (word-at-point)))
+                          ""))))))
     (let ((erc-buffers nil))
       (dolist (buffer (buffer-list))
         (with-current-buffer buffer
