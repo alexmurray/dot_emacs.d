b3fe329a380f0236a4e0d7e1822a768c59348ba0
Alex Murray <murray.alex@gmail.com>
2020-07-07 20:45:29 +0930

    Don't need to refile 10 deep :)

--- init.el
+++ init.el
@@ -1899,7 +1899,7 @@ With a prefix argument, will default to looking for all
         org-todo-keywords '((sequence "TODO(t)" "IN-PROGRESS(p!)" "BLOCKED(b@)" "DEFERRED(D@)" "|" "DONE(d!)")
                             (sequence "|" "CANCELLED(c@)" "DELEGATED(G@)")))
   ;; allow to refile to anywhere
-  (setq org-refile-targets '((nil :maxlevel . 10)))
+  (setq org-refile-targets '((nil :maxlevel . 4)))
   (add-to-list 'org-file-apps '("\\.webm\\'" . "xdg-open %s"))
   (add-to-list 'org-file-apps '("\\.aup\\'" . "audacity %s"))
   ;; set up org-babel integration for plantuml
