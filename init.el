b09dd9b7d2a2918a1ab0da80e07a5e7016cbba9f
Alex Murray <murray.alex@gmail.com>
2020-09-07 14:16:28 +0930

    Integrate gnus with dired

--- init.el
+++ init.el
@@ -1213,6 +1213,24 @@ With a prefix argument, will default to looking for all
   ;; disable image loading
   (setq gnus-inhibit-images t))
 
+(use-package gnus-dired
+  :ensure gnus
+  :config
+  ;; make the `gnus-dired-mail-buffers' function also work on
+  ;; message-mode derived modes, such as mu4e-compose-mode
+  (define-advice gnus-dired-mail-buffers (:around (orig-fun &rest args) include-message-mode-derived-buffers)
+    "Return a list of active message buffers."
+    (let (buffers)
+      (save-current-buffer
+        (dolist (buffer (buffer-list t))
+          (set-buffer buffer)
+          (when (and (derived-mode-p 'message-mode)
+                     (null message-sent-message-via))
+            (push (buffer-name buffer) buffers))))
+      (append (apply orig-fun args) (nreverse buffers))))
+  (setq gnus-dired-mail-mode 'mu4e-user-agent)
+  (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode))
+
 (use-package go-mode
   :ensure t)
 
