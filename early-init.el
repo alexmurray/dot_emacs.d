1d0ed5a6b248004fd8594fc10f8196398c66fdec
Alex Murray <murray.alex@gmail.com>
2021-09-20 15:04:50 +0930

    Use system monospace font

--- early-init.el
+++ early-init.el
@@ -1,9 +1,14 @@
+;;; early-init --- Early initialization
+;;; Commentary:
+;;; Code:
 ;; Used in emacs 27 to speed up initial package loading
 (setq package-quickstart t)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-(let ((font "DejaVu Sans Mono-10"))
-  (add-to-list 'default-frame-alist `(font . ,font))
-  (set-frame-font font))
+;; use the system monospace font
+(setq-default font-use-system-font t)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+
+(provide 'early-init)
+;;; early-init.el ends here
