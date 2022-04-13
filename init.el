5175da3f0a81bbb79e6c724c3d5adc41190af442
Alex Murray <murray.alex@gmail.com>
2022-04-13 12:04:15 +0930

    Add support for bug-reference-mode to link-hint
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- init.el
+++ init.el
@@ -1483,7 +1483,37 @@ With a prefix argument, will default to looking for all
   :ensure t
   :bind
   ("C-c C-l C-o" . link-hint-open-link)
-  ("C-c C-l C-c" . link-hint-copy-link))
+  ("C-c C-l C-c" . link-hint-copy-link)
+  :config
+  (defun link-hint--next-bug-reference (bound)
+    "Find the next bug-reference."
+    (let ((next (next-single-char-property-change
+                 (point) 'bug-reference-url nil bound)))
+      (unless (eq next bound)
+        ;; check 'bug-reference-url is set at next - if it is then return
+        ;; this
+        (catch 'found
+          (dolist (overlay (overlays-at next))
+            (when (overlay-get overlay 'bug-reference-url)
+              (throw 'found next)))
+          ;; otherwise find the next one
+          (setq next
+                (next-single-char-property-change
+                 next 'bug-reference-url nil bound))
+          (unless (eq next bound) next)))))
+
+  (defun link-hint--bug-reference-at-point-p ()
+    "Return the bug-reference url at the point or nil."
+    (car (get-char-property-and-overlay (point) 'bug-reference-url)))
+
+  (link-hint-define-type 'bug-reference
+    :next #'link-hint--next-bug-reference
+    :at-point-p #'link-hint--bug-reference-at-point-p
+    :vars '(bug-reference-mode)
+    :open #'browse-url
+    :copy #'kill-new)
+
+  (push 'link-hint-bug-reference link-hint-types))
 
 (use-package lxd-tramp
   :ensure t)
