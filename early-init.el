768136c145b54c1562af75c9df0a49dd5c35b975
Alex Murray <murray.alex@gmail.com>
2024-03-13 22:53:57 +1030

    early-init.el: disable package-quickstart since causes issues
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- early-init.el
+++ early-init.el
@@ -1,8 +1,9 @@
 ;;; early-init --- Early initialization
 ;;; Commentary:
 ;;; Code:
-;; Used in emacs 27 to speed up initial package loading
-(setq package-quickstart t)
+;; Used in emacs 27 to speed up initial package loading but causes issues when
+;; upgrading / installing new packages so disabled
+;; (setq package-quickstart t)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;; use the system monospace font
 (setq-default font-use-system-font t)
